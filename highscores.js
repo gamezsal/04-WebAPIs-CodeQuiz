e7c7789b5d89eab2585662c98bb1483c62e68187
Sal Gamez <gamezsalvador@gmail.com>
2020-10-10 22:37:01 -0600

    Added the ability to display highscores in highscore.html

--- highscores.js
+++ highscores.js
@@ -1,6 +1,11 @@
 var highScoresList = document.getElementById('highScoresList');
 var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
 
-console.log(highscores);
+
+highscores.innerHTML = highscores
+.map(score => {
+    return '<li class="high-scores">${username.score}-${score.score}</li>';
+})
+.join("");
 
 
